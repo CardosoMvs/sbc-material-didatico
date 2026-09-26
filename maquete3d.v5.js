df49564b3af5d6060f17c779357ea76db417263d
Marcos Cardoso <127773820+CardosoMvs@users.noreply.github.com>
2026-09-26 02:02:25 -0300

    Update maquete3d.v5.js

--- maquete3d.v5.js
+++ maquete3d.v5.js
@@ -593,9 +593,9 @@ var Maquete3D = (function () {
             tufo: perturbarGeo(new THREE.SphereGeometry(0.15, 7, 6), 0.04, 41),
             capim: perturbarGeo(new THREE.ConeGeometry(0.09, 0.34, 7), 0.03, 42),
             capimAlto: perturbarGeo(new THREE.ConeGeometry(0.10, 0.62, 8), 0.035, 421),
-            palha: perturbarGeo(new THREE.CylinderGeometry(0.018, 0.022, 0.46, 7), 0.02, 43),
-            palhaCurta: perturbarGeo(new THREE.CylinderGeometry(0.015, 0.018, 0.28, 7), 0.02, 44),
-            palhaDeitada: perturbarGeo(new THREE.CylinderGeometry(0.018, 0.022, 0.62, 7), 0.025, 431),
+            palha: perturbarGeo(new THREE.CylinderGeometry(0.016, 0.020, 0.52, 7), 0.02, 43),
+            palhaCurta: perturbarGeo(new THREE.CylinderGeometry(0.014, 0.017, 0.32, 7), 0.02, 44),
+            palhaDeitada: perturbarGeo(new THREE.CylinderGeometry(0.015, 0.019, 0.72, 7), 0.025, 431),
             flor: perturbarGeo(new THREE.SphereGeometry(0.055, 7, 6), 0.02, 45),
             haste: new THREE.CylinderGeometry(0.018, 0.028, 0.58, 7),
             folhaTrigo: perturbarGeo(new THREE.CylinderGeometry(0.012, 0.018, 0.72, 7), 0.02, 452),
@@ -679,15 +679,17 @@ var Maquete3D = (function () {
         for (var i = 0; i < qtd; i++) {
             var x = r.x0 + r2() * (r.x1 - r.x0);
             var z = r.z0 + r2() * (r.z1 - r.z0);
-            var ax = (r2() - 0.5) * (inclinacao || 0.35);
-            var az = (r2() - 0.5) * (inclinacao || 0.35);
-            var escala = 0.8 + r2() * 0.5;
-            var sy = comprimento ? comprimento * (0.8 + r2() * 0.4) : 1;
-            // achata no Y para parecer deitada e escala horizontalmente
+            // rotação quase horizontal, quase colada ao solo
+            var ax = Math.PI / 2 + (r2() - 0.5) * 0.25;
+            var az = (r2() - 0.5) * (inclinacao || 0.18);
+            var ry = r2() * Math.PI;
+            var escala = 0.9 + r2() * 0.5;
+            var sy = comprimento ? comprimento * (0.9 + r2() * 0.3) : 1;
+            // achata muito no Y para ficar bem deitada e alonga horizontalmente
             itens.push({
-                p: [x, altura(x, z) + 0.02, z],
-                r: [ax, r2() * Math.PI, az],
-                s: [escala * 1.4, sy * 0.25, escala * 1.4]
+                p: [x, altura(x, z) + 0.008, z],
+                r: [ax, ry, az],
+                s: [escala * 1.7, sy * 0.12, escala * 1.7]
             });
         }
         return itens;
@@ -713,10 +715,12 @@ var Maquete3D = (function () {
         g.add(instanciar(GE.milho, 0x7cb342, milhoItens, true));
 
         // palhada progressiva: 0-10% / ~40% / >90% de cobertura
-        var densPalha = [160, 520, 1300][ano - 1];
-        var comprPalha = [0.65, 0.85, 1.05][ano - 1];
-        g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T1, densPalha, comprPalha, 11 + ano, 0.9), false));
-        g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T1, Math.floor(densPalha * 0.6), 0.55, 111 + ano, 1.1), false));
+        var densPalha = [320, 1040, 2600][ano - 1];
+        var comprPalha = [0.80, 1.05, 1.35][ano - 1];
+        g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T1, densPalha, comprPalha, 11 + ano, 0.7), false));
+        g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T1, Math.floor(densPalha * 0.8), 0.70, 111 + ano, 0.9), false));
+        // palhada extra horizontal para reforçar cobertura
+        g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T1, Math.floor(densPalha * 0.6), comprPalha * 0.85, 311 + ano, 0.6), false));
 
         return g;
     }
@@ -806,7 +810,9 @@ var Maquete3D = (function () {
             });
 
             // pouca palhada: 0-10% de cobertura
-            g.add(instanciar(GE.palhaDeitada, 0xcbb26a, palhada(T2, 90, 0.55, 221, 0.8), false));
+            g.add(instanciar(GE.palhaDeitada, 0xcbb26a, palhada(T2, 140, 0.65, 221, 0.7), false));
+            // palhada de transição bem fina
+            g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T2, 100, 0.45, 1221, 0.8), false));
         } else {
             // contorno: fileiras seguem as curvas de nível dos morros
             // soja (safra) em anéis internos e milho (safrinha) em anéis externos, para não parecer plantio misturado
@@ -842,8 +848,10 @@ var Maquete3D = (function () {
 
             if (ano === 2) {
                 // palhada intermediária (~40%)
-                g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T2, 360, 0.75, 222, 0.9), false));
-                g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T2, 220, 0.5, 1222, 1.0), false));
+                g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T2, 540, 0.95, 222, 0.8), false));
+                g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T2, 330, 0.6, 1222, 0.9), false));
+                // palhada extra deitada
+                g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T2, 360, 0.85, 2222, 0.6), false));
             } else {
                 // ano 3: braquiária densa fora dos morros (mantém separação soja/milho)
                 var braq = [];
@@ -859,8 +867,10 @@ var Maquete3D = (function () {
                 }
                 g.add(instanciar(GE.braquiaria, 0x3a8f3a, braq, true));
                 // palhada densa (>90%)
-                g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T2, 900, 1.05, 223, 1.0), false));
-                g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T2, 500, 0.55, 1223, 1.1), false));
+                g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T2, 1350, 1.35, 223, 0.9), false));
+                g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T2, 750, 0.70, 1223, 1.0), false));
+                // camada extra de palhada bem deitada para cobertura total
+                g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T2, 900, 1.20, 3223, 0.6), false));
             }
         }
         return g;
@@ -896,7 +906,9 @@ var Maquete3D = (function () {
             }
             g.add(instanciar(GE.crotalaria, 0xf4d03f, flores, true));
             // pouca palhada no primeiro ano de transição
-            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 200, 0.65, 331, 0.8), false));
+            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 300, 0.80, 331, 0.7), false));
+            // palhada de transição
+            g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T3, 210, 0.55, 1331, 0.8), false));
         } else if (ano === 2) {
             // soja + milheto + trigo em fileiras intercaladas
             var soja2 = [], milheto = [], trigo2 = [];
@@ -917,8 +929,10 @@ var Maquete3D = (function () {
             g.add(instanciar(GE.trigo, 0xd9b64e, trigo2, true));
             g.add(instanciar(GE.milho, 0xc7c46a, milheto, true));
             // palhada intermediária
-            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 420, 0.85, 332, 0.9), false));
-            g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T3, 260, 0.5, 1332, 1.0), false));
+            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 630, 1.10, 332, 0.8), false));
+            g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T3, 390, 0.65, 1332, 0.9), false));
+            // palhada extra deitada
+            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 420, 1.0, 2332, 0.6), false));
         } else {
             // consórcio braquiária-milho: milho em fileiras com braquiária entrelinhas
             var milho3 = [];
@@ -929,8 +943,10 @@ var Maquete3D = (function () {
             }
             g.add(instanciar(GE.milho, 0x6a9a3a, milho3, true));
             // palhada densa característica do sistema avançado
-            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 800, 1.05, 333, 1.0), false));
-            g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T3, 450, 0.55, 1333, 1.1), false));
+            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 1200, 1.40, 333, 0.9), false));
+            g.add(instanciar(GE.palhaCurta, 0xcbb26a, palhada(T3, 675, 0.72, 1333, 1.0), false));
+            // palhada extra bem horizontal para reforçar cobertura
+            g.add(instanciar(GE.palhaDeitada, 0xd9c27e, palhada(T3, 800, 1.25, 2333, 0.6), false));
         }
         return g;
     }
